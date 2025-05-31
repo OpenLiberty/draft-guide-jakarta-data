0ccf23c09517d1b2c1db1576b0282445d1d0185e
Mark Swatosh <mark.swatosh@gmail.com>
2025-05-30 21:00:25 -0500

    Linting fixes

--- finish/src/test/java/it/io/openliberty/guides/data/DataGuideIT.java
+++ finish/src/test/java/it/io/openliberty/guides/data/DataGuideIT.java
@@ -1,13 +1,14 @@
+// tag::copyright[]
 /*******************************************************************************
-* Copyright (c) 2025 IBM Corporation and others.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v2.0
-* which accompanies this distribution, and is available at
-* https://www.eclipse.org/legal/epl-v20.html
-*
-* Contributors:
-*     IBM Corporation - initial API and implementation
-*******************************************************************************/
+ * Copyright (c) 2025 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+// end::copyright[]
 package it.io.openliberty.guides.data;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,14 +19,14 @@ import org.junit.jupiter.api.Test;
 
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.Response;
 
 
 public class DataGuideIT {
 
     private static final String PORT = System.getProperty("http.port");
-    private static final String URL = "http://localhost:" + PORT + "/shipping/packageQuery";
+    private static final String URL = "http://localhost:" + PORT 
+                                        + "/shipping/packageQuery";
 
     private Client client;
 
@@ -42,13 +43,10 @@ public class DataGuideIT {
 
     @Test
     public void testQueries() throws Exception {
-        Client client = ClientBuilder.newClient();
-        WebTarget target = client.target(URL);
-        Response response = target.request().get();
+        Response response = client.target(URL).request().get();
         assertEquals(200, response.getStatus(),
-            "Incorrect response code from " + target.getUri().getPath());
+            "Incorrect response code from: " + URL);
 
     }
 
-    
 }
